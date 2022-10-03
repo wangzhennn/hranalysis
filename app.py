a7e50e22f1a76604ade69d7b5975c83351f998ca
wangzhen <101124460+wangzhennn@users.noreply.github.com>
2022-10-03 15:37:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-#pip install umap-learn
-import umap.umap_ as umap
-
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import altair as alt
-
+import umap.umap_ as umap
 import seaborn as sns
 sns.set()
 
 hru=pd.read_csv("hru.csv")
 
-hru.head(5)
+print(hru.head(5))
 
 pd.crosstab(hru.job_level,hru.stock_option)
 
