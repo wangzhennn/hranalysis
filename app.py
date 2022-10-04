c08a433aa7535ee3e239110a7b7c408869f9dda2
wangzhen <101124460+wangzhennn@users.noreply.github.com>
2022-10-04 17:19:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # ---- INPUT FOR THE RECRUITMENT EVALUATION ----
-st.text_input('Name','Zhen Wang')
+name=st.text_input('Name','Zhen Wang')
 st.select_slider('Choose',[1,2,3,4,5])
-st.number_input('Age',0,100)
+age=st.number_input('Age',0,100)
+xx=st.selectbox('xx',["S","C"])
