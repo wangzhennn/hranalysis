fb58a6a78322132960dbd5034e6d13f2d0ddf4e5
wangzhen <101124460+wangzhennn@users.noreply.github.com>
2022-10-04 22:15:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,6 @@ if st.button('Predict! 🚀'):
                                'gender':gender,
                                'job_role':job_role,
                                'marital':marital}, index=[0])
-            ohe = OneHotEncoder(sparse=False).fit(new_df_cat)
             new_values_cat = pd.DataFrame(ohe.transform(new_df_cat),columns=['buiness_travel', 'gender', 'job_role', 'marital'],index=[0])
             
             line_to_pred = pd.concat([new_values_num, new_values_cat], axis=1)
