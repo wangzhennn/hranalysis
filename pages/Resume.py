a2c082198f497bc8b2cf3a0f1611be655f735297
wangzhen <101124460+wangzhennn@users.noreply.github.com>
2022-10-05 13:36:39 +0200

    Update Resume.py

--- pages/Resume.py
+++ pages/Resume.py
@@ -17,14 +17,8 @@ DESCRIPTION = """
 Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
 """
 EMAIL = "johndoe@email.com"
-SOCIAL_MEDIA = {
-    "YouTube": "https://youtube.com/c/codingisfun",
-    "LinkedIn": "https://linkedin.com",
-    "GitHub": "https://github.com",
-    "Twitter": "https://twitter.com",
-}
 PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
+    "🏆 st.write("you have entered", st.session_state["my_input_1"])",
     "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
     "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
     "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
